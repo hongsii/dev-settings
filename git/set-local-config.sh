9d2618de473491ba7af090a3c78fb6209afddd01
hongsii <hongsii.dev@gmail.com>
2019-04-14 15:18:18 +0900

    Git 로컬 설정 스크립트 추가
    - alias가 등록됐는지 분기처리가 필요

--- git/set-local-config.sh
+++ git/set-local-config.sh
@@ -1,10 +1,34 @@
 #! /bin/bash
 
+# Check installed git
+if ! [ -x "$(command -v git)" ]; then
+    echo "you must install git !"
+fi
+
+# Notificate alias
+alias_command=local-git
+if [ `alias | grep which | wc -l` == 0 ]; then
+    echo "Add alias for easy use"
+    echo "$ alias $alias_command=`pwd`/`basename $0`"
+fi
+
+# Check current directory
+git_dir=$1
+if ! [ $git_dir ]; then
+    git_dir=`pwd`
+    echo -e "\n===== Current directory : $git_dir ====="
+fi
+
+# Set local config
 username=hongsii
 email=hongsii.dev@gmail.com
 
-git_path=$1
-cd $1
-echo "change local config to username : $username / email : $email"
+echo ""
+echo "===== Set user.name ====="
+git config --local --unset-all user.name
 git config --local --add user.name $username
+git config --local --list | grep "user.name"
+echo "===== Set user.email ====="
+git config --local --unset-all user.email
 git config --local --add user.email $email
+git config --local --list | grep "user.email"
