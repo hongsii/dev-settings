b8ff840895f476ca72b748fdc9cc2a7ea9437f5d
hongsii <hongsii.dev@gmail.com>
2019-09-28 22:56:58 +0900

    brew : brew install 스크립트 수정
    - brew가 설치돼있으면 실행하지 않도록 수정

--- mac/install_brew.sh
+++ mac/install_brew.sh
@@ -1,3 +1,8 @@
+if [ -x "$(command -v brew)" ]; then
+    echo "Already install brew. skip this script"
+    exit 1
+fi
+
 echo "Install brew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
