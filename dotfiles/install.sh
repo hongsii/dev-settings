348348e26082d94e81e790889a6995e7bdca2fca
hongsii <hongsii.dev@gmail.com>
2019-12-10 22:16:24 +0900

    dotfiles : brew 추가

---
+++ dotfiles/install.sh
@@ -0,0 +1,19 @@
+if [ -x "$(command -v brew)" ]; then
+    echo "Already install brew. skip this script"
+    exit 1
+fi
+
+echo "Install brew"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew_file_name="BrewFile"
+if [ ! -f "$brew_file_name" ]; then
+    echo "$brew_file_name doesn't exist"
+    exit 1
+fi
+
+brew update
+brew install mas
+brew bundle --file=./BrewFile
+brew cleanup
+brew csak cleanup
